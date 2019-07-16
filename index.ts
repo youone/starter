52df46ec50eb513c4ecf1eef78b8947c1aa801d2
Johan Lundquist <jolundq@gmail.com>
2019-07-16 21:37:43 +0200

    wip

---
+++ index.ts
@@ -0,0 +1,13 @@
+// import $ from 'jquery'; //in TypeScript jquery must be imported
+// import './css/styles.css';
+// import '../assets/images/nodejslogo.png';
+// import packageInfo from '../package.json';
+//
+// console.info('main.ts LOADED!');
+// console.info(packageInfo.name, packageInfo.version);
+// console.log($('body'));
+
+export function exportedFunction() {
+    console.log('exported function in nstart');
+}
+
